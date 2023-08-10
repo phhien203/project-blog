6dd5894962157e395c85f0bf690f0be8aa224573
Pham Huu Hien <phhien203@gmail.com>
2023-08-10 16:51:17 +0700

    implement theme switcher

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,38 @@
-import React from 'react';
+'use client';
+
 import clsx from 'clsx';
-import { Rss, Sun, Moon } from 'react-feather';
+import React from 'react';
+import { Moon, Rss, Sun } from 'react-feather';
 
 import Logo from '@/components/Logo';
 import VisuallyHidden from '@/components/VisuallyHidden';
 
+import { DARK_TOKENS, LIGHT_TOKENS } from '@/constants';
+import Cookies from 'js-cookie';
 import styles from './Header.module.css';
 
-function Header({ theme, className, ...delegated }) {
+function Header({ theme: initialTheme, className, ...delegated }) {
+  const [theme, setTheme] = React.useState(initialTheme);
+
+  function switchTheme() {
+    const nextTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(nextTheme);
+
+    Cookies.set('color-theme', nextTheme, { expires: 1000 });
+
+    const COLORS = nextTheme === 'light' ? LIGHT_TOKENS : DARK_TOKENS;
+
+    const root = document.documentElement;
+
+    root.setAttribute('data-color-theme', nextTheme);
+
+    for (const [key, value] of Object.entries(COLORS)) {
+      root.style.setProperty(key, value);
+    }
+  }
+
   return (
-    <header
-      className={clsx(styles.wrapper, className)}
-      {...delegated}
-    >
+    <header className={clsx(styles.wrapper, className)} {...delegated}>
       <Logo />
 
       <div className={styles.actions}>
@@ -24,15 +44,12 @@ function Header({ theme, className, ...delegated }) {
               transform: 'translate(2px, -2px)',
             }}
           />
-          <VisuallyHidden>
-            View RSS feed
-          </VisuallyHidden>
+          <VisuallyHidden>View RSS feed</VisuallyHidden>
         </button>
-        <button className={styles.action}>
-          <Sun size="1.5rem" />
-          <VisuallyHidden>
-            Toggle dark / light mode
-          </VisuallyHidden>
+
+        <button className={styles.action} onClick={() => switchTheme()}>
+          {theme === 'light' ? <Sun size="1.5rem" /> : <Moon size="1.5rem" />}
+          <VisuallyHidden>Toggle dark / light mode</VisuallyHidden>
         </button>
       </div>
     </header>
