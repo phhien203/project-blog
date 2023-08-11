73293639da22e9bf38bd18601d110b71664b280a
Pham Huu Hien <phhien203@gmail.com>
2023-08-11 15:16:17 +0700

    add CircularColorsDemo component

--- src/helpers/mdx-components.js
+++ src/helpers/mdx-components.js
@@ -1,7 +1,9 @@
+import CircularColorsDemo from '@/components/CircularColorsDemo';
 import CodeSnippet from '@/components/CodeSnippet/CodeSnippet';
 import DivisionGroupsDemo from '@/components/DivisionGroupsDemo';
 
 export const MDX_COMPONENTS_MAP = {
   pre: CodeSnippet,
   DivisionGroupsDemo: DivisionGroupsDemo,
+  CircularColorsDemo: CircularColorsDemo,
 };
