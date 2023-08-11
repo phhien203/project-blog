c2b46cb5349529c948c717c5cb8f23fc378216b0
Pham Huu Hien <phhien203@gmail.com>
2023-08-11 15:44:11 +0700

    add RSS feed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -36,7 +36,7 @@ function Header({ theme: initialTheme, className, ...delegated }) {
       <Logo />
 
       <div className={styles.actions}>
-        <button className={styles.action}>
+        <a href="/rss.xml" className={styles.action}>
           <Rss
             size="1.5rem"
             style={{
@@ -45,7 +45,7 @@ function Header({ theme: initialTheme, className, ...delegated }) {
             }}
           />
           <VisuallyHidden>View RSS feed</VisuallyHidden>
-        </button>
+        </a>
 
         <button className={styles.action} onClick={() => switchTheme()}>
           {theme === 'light' ? <Sun size="1.5rem" /> : <Moon size="1.5rem" />}
